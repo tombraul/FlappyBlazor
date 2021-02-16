a9c052cf1c6b717e9c1d55d5f9a950357ceb16be
Tom Braul <tobr772@gmail.com>
2021-02-16 15:12:18 +0100

    Flappy Blazor part 2

---
+++ FlappyBlazor/Models/Pipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBlazor.Models
+{
+    public class Pipe
+    {
+        public int DistanceFromLeft { get; private set; } = 500;
+        public int DistanceFromBottom { get; private set; } = new Random().Next(0, 60);
+    }
+}
